144c52aab7178e8558f950dc4de47b24786250dd
Gus <gustavo.m.tijerino@gmail.com>
2025-05-04 17:47:32 -0700

    Added Initial CSS and testing logic

--- script/logic.js
+++ script/logic.js
@@ -1,1 +1,22 @@
-console.log(4);
+
+const counter = document.querySelector(".counter")
+const buttons = document.querySelectorAll("button");
+let nums = []
+buttons.forEach(function (button) {
+
+    button.addEventListener('click', function () {
+        let btnValue = button.textContent.trim()
+        if (btnValue !== "C" && btnValue !== "Bck") {
+
+            nums.push(btnValue);
+            counter.innerHTML = nums.join("")
+        }
+
+    });
+
+});
+
+
+
+
+
